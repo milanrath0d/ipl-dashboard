2781ebbc3747cbae1268c1fd2754c3dadbf32960
Milan Rathod <milan.rathod@gdn-commerce.com>
2023-02-26 18:58:08 +0530

    added react js files

---
+++ src/ui/src/App.js
@@ -0,0 +1,25 @@
+import './App.scss';
+import {TeamPage} from './pages/TeamPage';
+import {HashRouter as Router, Route, Routes} from 'react-router-dom';
+import {MatchPage} from "./pages/MatchPage";
+import {HomePage} from "./pages/HomePage";
+
+function App() {
+    return (
+        <div className="App">
+            <Router>
+                <Routes>
+                    <Route path="/teams/:teamName" element={<TeamPage/>}/>
+                </Routes>
+                <Routes>
+                    <Route path="/teams/:teamName/matches/:year" element={<MatchPage/>}/>
+                </Routes>
+                <Routes>
+                    <Route path="/" element={<HomePage/>}/>
+                </Routes>
+            </Router>
+        </div>
+    );
+}
+
+export default App;
